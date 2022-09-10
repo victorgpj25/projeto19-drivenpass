1478626dfb298c60676865db962911525bd3bff2
Victor <victorgpj25@gmail.com>
2022-09-10 15:14:32 -0300

    signUp: controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+
+import * as userService from "../services/userService"
+
+export async function signUp(req: Request, res: Response) {
+    const { email, password }: {email: string, password: string } = req.body
+    await userService.signUp({email, password})
+
+    res.sendStatus(201)
+}
+
