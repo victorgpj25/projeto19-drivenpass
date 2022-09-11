38fdfa37f849a39cf9b3c0ddf2b78e260b89fc10
Victor <victorgpj25@gmail.com>
2022-09-11 14:45:48 -0300

    getCredentials: routes

--- src/routes/credentialRouter.ts
+++ src/routes/credentialRouter.ts
@@ -1,11 +1,20 @@
 import express from 'express'
 
-import { insertCredential } from '../controllers/credentialController'
-import { validateInsertCredentialReqBody } from '../middlewares/credentialMiddleware'
+import { 
+    insertCredential,
+    getCredentials,
+    getCredentialsById
+} from '../controllers/credentialController'
+import { 
+    validateInsertCredentialReqBody, 
+    validateCredentialsReqParams 
+} from '../middlewares/credentialMiddleware'
 import { verifyToken } from '../middlewares/authMiddleware'
 
 const credentialRouter = express.Router()
 
 credentialRouter.post('/credentials', verifyToken, validateInsertCredentialReqBody, insertCredential)
+credentialRouter.get('/credentials', verifyToken, getCredentials)
+credentialRouter.get('/credentials/:id', verifyToken, validateCredentialsReqParams, getCredentialsById)
 
 export default credentialRouter
