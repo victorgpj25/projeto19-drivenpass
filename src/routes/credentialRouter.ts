7fbfb79fdd9b2c032e82282e437ab4e29fee1cff
Victor <victorgpj25@gmail.com>
2022-09-11 15:37:46 -0300

    deleteCredentials: route

--- src/routes/credentialRouter.ts
+++ src/routes/credentialRouter.ts
@@ -3,7 +3,8 @@ import express from 'express'
 import { 
     insertCredential,
     getCredentials,
-    getCredentialsById
+    getCredentialsById,
+    deleteCredentials
 } from '../controllers/credentialController'
 import { validateInsertCredentialReqBody } from '../middlewares/credentialMiddleware'
 import { verifyToken } from '../middlewares/authMiddleware'
@@ -14,5 +15,6 @@ const credentialRouter = express.Router()
 credentialRouter.post('/credentials', verifyToken, validateInsertCredentialReqBody, insertCredential)
 credentialRouter.get('/credentials', verifyToken, getCredentials)
 credentialRouter.get('/credentials/:id', verifyToken, validateReqParamsId, getCredentialsById)
+credentialRouter.delete('/credentials/:id', verifyToken, deleteCredentials)
 
 export default credentialRouter
