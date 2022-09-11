1a8cc57425a23227f04ee03140088d1f11307722
Victor <victorgpj25@gmail.com>
2022-09-11 16:40:38 -0300

    getNotes: routes

--- src/routes/noteRouter.ts
+++ src/routes/noteRouter.ts
@@ -1,11 +1,18 @@
 import express from 'express'
 
-import { insertNote } from '../controllers/noteController'
+import { 
+    insertNote, 
+    getNotes,
+    getNotesById 
+} from '../controllers/noteController'
 import { validatePostNoteReqBody } from '../middlewares/noteMiddleware'
 import { verifyToken } from '../middlewares/authMiddleware'
+import { validateReqParamsId } from '../middlewares/reqParamsMiddleware'
 
 const noteRouter = express.Router()
 
 noteRouter.post('/notes', verifyToken, validatePostNoteReqBody, insertNote)
+noteRouter.get('/notes', verifyToken, getNotes)
+noteRouter.get('/notes/:id', verifyToken, validateReqParamsId, getNotesById)
 
 export default noteRouter
