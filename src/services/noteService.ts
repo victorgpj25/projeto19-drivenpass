8d9bad6a73b8dacb77fa70927fec2c71d1358b27
Victor <victorgpj25@gmail.com>
2022-09-11 16:45:23 -0300

    getNotes: services

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,9 +1,20 @@
 import * as noteRepository from '../repositories/noteRepository'
 
-import * as noteUtils from "../utils/credentialUtils"
+import * as noteUtils from "../utils/noteUtils"
 
 export async function insertNote(userId: number, tag: string, note: string) {
     await noteUtils.verifyTagConflict(userId, tag)
 
     await noteRepository.insert({userId, tag, note})
+}
+
+export async function getNotes(userId: number) {
+    const notes = await noteRepository.findByUserId(userId)
+    return notes
+}
+
+export async function getnotesById(userId: number, noteId: number) {
+    const note = await noteRepository.findByNoteId(userId, noteId)
+    if (!note) throw {code: 'search_failed', message: 'Given id does not exist or belongs to another user'}
+    return note
 }
